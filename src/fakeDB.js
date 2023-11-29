aa0619f92e87c5b04662406a8ed6eae673dbfafb
LazyMG <cbfmark@gmail.com>
2023-11-29 18:22:40 +0900

    fakeDB

---
+++ src/fakeDB.js
@@ -0,0 +1,42 @@
+const musics = [
+  {
+    id: 1,
+    singer: "아이묭",
+    singerEng: "",
+    title: "",
+    titleEng: "",
+    albumTitle: "",
+    coverImg: "/images/Aimyon-Excitement_of_Youth.jpg",
+  },
+  {
+    id: 2,
+    singer: "아이묭",
+    singerEng: "",
+    title: "",
+    titleEng: "",
+    albumTitle: "",
+    coverImg: "/images/Aimyon-Falling_into_your_eyes_Record.jpg",
+  },
+  {
+    id: 3,
+    singer: "찰리 푸스",
+    singerEng: "Charlie Puth",
+    title: "",
+    titleEng: "",
+    albumTitle: "",
+    coverImg: "/images/Charlie_Puth-Nine_Track_Mind.jpg",
+  },
+  {
+    id: 4,
+    singer: "검정치마",
+    singerEng: "The Black Skirts",
+    title: "Antifreeze",
+    titleEng: "Antifreeze",
+    albumTitle: "",
+    coverImg: "/images/The_Black_Skirts-201.jpg",
+  },
+];
+
+const getMusics = () => musics;
+
+export default getMusics;
--- src/server.js
+++ src/server.js
@@ -1,19 +1,25 @@
 import express from "express";
+import getMusics from "./fakeDB";
+import path from "path";
 
 const PORT = 4000;
 const app = express();
 
-const musicArr = [
-  { title: "First music", singer: "Anon", duration: "3.4" },
-  { title: "Second music", singer: "Anon", duration: "3" },
-  { title: "Thrid music", singer: "Anon", duration: "3.5" },
-];
+app.use(express.static(path.join(process.cwd(), "/public")));
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.get("/", (req, res) =>
-  res.render("home", { pageTitle: "Home", musics: musicArr })
+  res.render("home", { pageTitle: "Home", musics: getMusics() })
 );
+app.get("/search", (req, res) => {
+  const { keyword } = req.query;
+  const result = getMusics().filter(
+    (music) =>
+      music.singer.includes(keyword) || music.singerEng.includes(keyword)
+  );
+  res.render("search", { pageTitle: "Search", musics: result });
+});
 
 app.listen(PORT, () => {
   console.log(`Visit http://localhost:${PORT} 🎈`);
